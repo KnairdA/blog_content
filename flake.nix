41014a0006f76aaec1e59259c2ece8602c539b4d
Adrian Kummerlaender <adrian@kummerlaender.eu>
2021-09-22 21:57:17 +0200

    Update CMS, add simple server for testing

--- flake.nix
+++ flake.nix
@@ -2,10 +2,27 @@
   description = "blog.kummerlaender.eu";
 
   inputs = {
+    stable.url = github:NixOS/nixpkgs/nixos-21.05;
     cms.url = git+https://code.kummerlaender.eu/blog.kummerlaender.eu;
   };
 
-  outputs = { self, cms, ... }: {
+  outputs = { self, stable, cms, ... }: {
     defaultPackage.x86_64-linux = cms.generate ./.;
+
+    defaultApp.x86_64-linux = let
+      system = "x86_64-linux";
+
+      pkgs = import stable { inherit system; };
+
+      serve = pkgs.writeScriptBin "serve" ''
+        #!/bin/sh
+        pushd ${cms.generate ./.}
+        ${pkgs.gatling}/bin/gatling
+        popd
+      '';
+    in {
+      type = "app";
+      program = "${serve}/bin/serve";
+    };
   };
 }
